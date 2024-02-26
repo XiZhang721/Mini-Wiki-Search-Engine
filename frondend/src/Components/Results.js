c95f5c1e556c3dc4f826617b37d23f944a956ad8
Stephen0623 <55511103+Stephen0623@users.noreply.github.com>
2024-02-26 00:36:53 +0000

    Add files via upload

---
+++ frondend/src/Components/Results.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ResultsPage() {
+    return (
+      <div>
+        <h2>Results</h2>
+        {/* Display your search results here */}
+      </div>
+    );
+  }
+
+export default ResultsPage;
