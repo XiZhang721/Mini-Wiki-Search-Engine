c78e8f71bb0decf2859a2b4fde0af49d5cf786fe
应新锐 <xinrui@yingxinruideMacBook-Air.local>
2024-03-10 11:32:25 +0000

    update results.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,22 +2,18 @@
 import { initializeApp } from "firebase/app";
 import{getAuth} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDtlQE-Y8lWjz0dFf-vKaNfxNV6EYcnQto",
-  authDomain: "ttds-test.firebaseapp.com",
-  projectId: "ttds-test",
-  storageBucket: "ttds-test.appspot.com",
-  messagingSenderId: "175574809982",
-  appId: "1:175574809982:web:6240d3ef35ae7c6a49472f",
-  measurementId: "G-403301Q7DW"
-};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+const app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ttds-412917-default-rtdb.europe-west1.firebasedatabase.app"
+});
 
 export const auth = getAuth(app);
 export default app;
